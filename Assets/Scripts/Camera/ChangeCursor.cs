e6a3f8cc124055c51c7d756707f319de5eff24ff
zaddirra <dautov.aa@bk.ru>
2021-11-05 11:42:34 +0300

    Сделали компоненты курсора

--- Assets/Scripts/Camera/ChangeCursor.cs
+++ Assets/Scripts/Camera/ChangeCursor.cs
@@ -4,20 +4,28 @@ using UnityEngine;
 
 public class ChangeCursor : MonoBehaviour
 {
+    [SerializeField]
     private Texture2D cursorDefault;
-    private Texture2D cursorChanged;
+    [SerializeField]
+    private Texture2D cursorAttack;
+    [SerializeField]
+    private Texture2D cursorPickUp;
 
-    void Start()
+ 
+    public void SetCursor(string tag)
+    {
+        if(tag == "Enemy")
+        {
+            Cursor.SetCursor(cursorAttack, Vector2.zero, CursorMode.ForceSoftware);
+        }
+        else if(tag == "PickUp") 
+        {
+            Cursor.SetCursor(cursorPickUp, Vector2.zero, CursorMode.ForceSoftware);
+        }
+    }
+
+    public void ResetCursor()
     {
         Cursor.SetCursor(cursorDefault, Vector2.zero, CursorMode.ForceSoftware);
     }
-    private void OnMouseEnter()
-    {
-        Cursor.SetCursor(cursorChanged, Vector2.zero, CursorMode.ForceSoftware);
-    }
-    private void OnMouseExit()
-    {
-        Cursor.SetCursor(cursorDefault, Vector2.zero, CursorMode.ForceSoftware);
-    }
-
 }
--- Assets/Scripts/PickUp/PickUp.cs
+++ Assets/Scripts/PickUp/PickUp.cs
@@ -2,19 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class PickUp : MonoBehaviour
 {
     ChangeCursor cursor;
     // Start is called before the first frame update
     void Start()
     {
         cursor = FindObjectOfType<ChangeCursor>();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 
     private void OnMouseEnter()
