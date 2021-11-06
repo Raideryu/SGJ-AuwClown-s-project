d0154d10cd5ce16dc4259230f8fa655631378598
zaddirra <dautov.aa@bk.ru>
2021-11-06 18:35:02 +0300

    chunck placer

--- Assets/Scripts/Generate/ChunkPlacer.cs
+++ Assets/Scripts/Generate/ChunkPlacer.cs
@@ -6,96 +6,75 @@ using System.Linq;
 
 public class ChunkPlacer : MonoBehaviour
 {
-    public Transform Player;
-    public Chunk[] ChunkPrefabs;
-    public Chunk[] TransitionPrefabs;
-    //public Chunk[] endsList;
+    [SerializeField] Chunk startRoomPrefab;
+    [SerializeField] Chunk finishRoomPrefab;
+    [SerializeField] Chunk[] ChunkPrefabs;
+    [SerializeField] Chunk[] TransitionPrefabs;
+
+    [SerializeField] int mainPathRoomsCount = 5;
+    [SerializeField] int tupikPathRoomsCount = 2;
+
     private List<Chunk> spawnedChunks = new List<Chunk>();
-    private List<Chunk> spawnedTransition = new List<Chunk>();
-    [SerializeField]
-    float DestroyDistance = 20, spawnDistance = 3;
+    //private List<Chunk> spawnedTransition = new List<Chunk>();
 
-    void Start()
+    private Chunk startRoom;
+    private Chunk finishRoom;
+
+    void GeneratePath()
     {
-        Player = FindObjectOfType<PlayerInput>().gameObject.transform;
+        foreach (Chunk chunks in spawnedChunks)
+            Destroy(chunks.gameObject);
+        spawnedChunks.Clear();
 
-        //SpawnChunk();
+
+        startRoom = SpawnChunk(transform, startRoom); // стартовая комната
+
+        spawnedChunks.Add(startRoom);
+
+        // спавню комнаты главного пути
+        for(int i=0;i< mainPathRoomsCount; i++)
+        {
+            int rndEnumb = Random.Range(0,spawnedChunks.Last().Exits.Length-1);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        List<Chunk> removeList = new List<Chunk>();
-        List<Transform> addList = new List<Transform>();
-
-        foreach (Chunk chunk in spawnedTransition)
-        {
-            if (Vector3.Distance(Player.transform.position, chunk.transform.position) > DestroyDistance)
-            {
-                removeList.Add(chunk);
-            }
-            else if (Vector3.Distance(Player.transform.position, chunk.Exits[0].transform.position) < spawnDistance)
-            {
-                //SpawnAllRooms(chunk.Exits[0]);
-                addList.Add(chunk.Exits[0]);
-                break;
-            }
-        }
-
-        //foreach
-
-        //foreach (Chunk chunk in spawnedTransition)
 
 
-        //    foreach (Transform exit in chunk.Exits)
-        //        foreach (Chunk chunk in spawnedChunks)
-        //            Destroy(chunk.gameObject);
 
-        //if (Input.GetKeyDown(KeyCode.Space))
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            SpawnChunk();
-        }
-        if (Input.GetKeyDown(KeyCode.Q))
-        {
-            Del();
-        }
 
-    }
+    //private void SpawnChunk()
+    //{
+    //    Chunk newChunk = Instantiate(ChunkPrefabs[Random.Range(0, ChunkPrefabs.Length - 1)], Vector3.zero, Quaternion.identity);
 
-    private void SpawnChunk()
-    {
-        Chunk newChunk = Instantiate(ChunkPrefabs[Random.Range(0, ChunkPrefabs.Length - 1)], Vector3.zero, Quaternion.identity);
+    //    foreach (var exit in newChunk.Exits)
+    //    {
+    //        SpawnAllRooms(exit);
+    //    }
+    //    spawnedChunks.Add(newChunk);
+    //}
+    //[SerializeField]
+    //int maxRooms = 10;
 
-        foreach (var exit in newChunk.Exits)
-        {
-            SpawnAllRooms(exit);
-        }
-        spawnedChunks.Add(newChunk);
-    }
-    [SerializeField]
-    int maxRooms = 10;
+    //void SpawnAllRooms(Transform exit)
+    //{
+    //    if (spawnedChunks.Count >= maxRooms) return;
 
-    void SpawnAllRooms(Transform exit)
-    {
-        if (spawnedChunks.Count >= maxRooms) return;
+    //    Chunk rndChunk = TransitionPrefabs[Random.Range(0, ChunkPrefabs.Length - 1)];
 
-        Chunk rndChunk = TransitionPrefabs[Random.Range(0, ChunkPrefabs.Length - 1)];
+    //    Chunk tr = Instantiate(rndChunk, exit.position + rndChunk.Enter.position, exit.rotation);
+    //    tr.transform.position = exit.position + tr.Enter.localPosition;
+    //    spawnedTransition.Add(tr);
 
-        Chunk tr = Instantiate(rndChunk, exit.position + rndChunk.Enter.position, exit.rotation);
-        tr.transform.position = exit.position + tr.Enter.localPosition;
-        spawnedTransition.Add(tr);
+    //    rndChunk = ChunkPrefabs[Random.Range(0, ChunkPrefabs.Length - 1)];
+    //    Chunk room = Instantiate(rndChunk, tr.Exits[0].position + rndChunk.Enter.position, tr.Exits[0].rotation);
+    //    //room.transform.position = tr.Exits[0].position + room.Enter.localPosition;
+    //    spawnedChunks.Add(room);
 
-        rndChunk = ChunkPrefabs[Random.Range(0, ChunkPrefabs.Length - 1)];
-        Chunk room = Instantiate(rndChunk, tr.Exits[0].position + rndChunk.Enter.position, tr.Exits[0].rotation);
-        //room.transform.position = tr.Exits[0].position + room.Enter.localPosition;
-        spawnedChunks.Add(room);
-
-        foreach (Transform _exit in room.Exits)
-        {
-            SpawnAllRooms(_exit);
-        }
-    }
+    //    foreach (Transform _exit in room.Exits)
+    //    {
+    //        SpawnAllRooms(_exit);
+    //    }
+    //}
 
 
     List<Chunk> SpawnOneRoom(Transform exit)
@@ -127,14 +106,7 @@ public class ChunkPlacer : MonoBehaviour
 
 
 
-    void Del()
-    {
-        foreach (Chunk chunk in spawnedTransition)
-            Destroy(chunk.gameObject);
-
-        foreach (Chunk chunk in spawnedChunks)
-            Destroy(chunk.gameObject);
-    }
+  
 
 
 }
