b2ca096b239576aa98988e8dd909024df3eca9a0
Raideryu <73846997+Raideryu@users.noreply.github.com>
2021-11-06 23:58:32 +0300

    Перенес UI Drawer в отдельный скрипт

---
+++ Assets/Scripts/UI/UIDrawer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIDrawer : MonoBehaviour
+{
+    public int maxHP;
+    public int actualHP;
+    public int numOfHearts;
+    public Image[] imgHeart;
+    public Sprite fullHeart;
+    public Sprite EmptyHeart;
+
+    public void DrawHP()
+    {
+
+    }
+}
